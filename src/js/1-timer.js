62c3719e471d1bfc1c35aad010c962176938f783
Nadiia-Yuce <159655565+Nadiia-Yuce@users.noreply.github.com>
2024-06-29 09:25:09 +0200

    1-start

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,29 @@
-console.log('- Hello, bitch!)');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const button = document.querySelector('button[data-start]');
+let userSelectedDate;
+
+button.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    const today = new Date();
+
+    if (selectedDates[0] > today) {
+      button.disabled = false;
+      userSelectedDate = selectedDates[0];
+    } else {
+      window.alert('Please choose a date in the future');
+      button.disabled = true;
+    }
+  },
+};
+
+const fp = flatpickr(input, options);
